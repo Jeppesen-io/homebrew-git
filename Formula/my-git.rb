84cd422bd1f517287c329ef9168746fcfb58bd38
Nelson Jeppesen <nelson@nelsonjeppeson-mbp.local>
2016-05-12 10:55:31 -0700

    Inital commit - My git!

---
+++ Formula/my-git.rb
@@ -0,0 +1,45 @@
+class MyGit < Formula
+  desc 'Install Git how I like it'
+  url 'https://github.com/Jeppesen-io/homebrew-git/archive/master.zip'
+  homepage 'https://github.com/Jeppesen-io/homebrew-git'
+  version '0.9.4'
+
+  depends_on 'git'
+  depends_on 'curl'
+
+  bottle :unneeded
+
+  def install
+
+    # Get home dir (stripped by homebrew)
+    home_dir = `sudo -Hu $USER bash -c 'echo -n $HOME'`
+
+    # Create xdg dirs for git
+    system 'mkdir', '-vp', "#{home_dir}/.config/git/"
+    system 'mkdir', '-vp', "#{home_dir}/.config/bash/"
+
+    # Prevent "empty install"
+    system 'touch'    , "#{prefix}/config"
+
+    # Use xdg config location
+    system 'touch'    , "#{home_dir}/.config/git/config"
+
+    system 'git','config','--global','--replace-all', 'alias.lol',     '"log --graph --decorate --oneline"'
+    system 'git','config','--global','--replace-all', 'alias.lola',    '"log --graph --decorate --oneline --all"'
+    system 'git','config','--global','--replace-all', 'alias.co',      '"checkout"'
+    system 'git','config','--global','--replace-all', 'alias.ct',      '"commit"'
+    system 'git','config','--global','--replace-all', 'alias.ps',      '"push"'
+    system 'git','config','--global','--replace-all', 'alias.pl',      '"pull"'
+    system 'git','config','--global','--replace-all', 'alias.dfm',     '"diff origin/master"'
+    system 'git','config','--global','--replace-all', 'alias.st',      'status'
+    system 'git','config','--global','--replace-all', 'alias.last',    '"log -1 HEAD"'
+    system 'git','config','--global','--replace-all', 'alias.some',    '"!git fetch -a && git pull"'
+    system 'git','config','--global','--replace-all', 'alias.rb',      '"rebase -i origin/master"'
+
+    # Bash completion
+    system 'curl', '-fLo', "#{home_dir}/.config/bash/git-completion.bash",
+      'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash'
+
+  end
+
+end
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # homebrew-git
+### Wraps `git` brew
 Git, for me!
+
+### Aliases
+    ct      commit
+    rb      interactive rebase
+    ps      push
+    pl      pull
+    co      checkout
+    co      checkout
+    dfm     diff from master
+    last    last change
+    some    pull all the fresh stuff down
+
