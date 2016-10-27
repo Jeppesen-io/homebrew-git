3a8272c7b1e03a9cd3bf81bb1c4a1f218909ee06
Nelson Jeppesen <Nelson@SimplyMeasured.com>
2016-10-27 11:49:23 -0700

    Add push.default and try different ways to get aliases working

--- Formula/my-git.rb
+++ Formula/my-git.rb
@@ -2,7 +2,7 @@ class MyGit < Formula
   desc 'Install Git how I like it'
   url 'https://github.com/Jeppesen-io/homebrew-git/archive/master.zip'
   homepage 'https://github.com/Jeppesen-io/homebrew-git'
-  version '0.9.4'
+  version '1.0.0'
 
   depends_on 'git'
   depends_on 'curl'
@@ -24,17 +24,21 @@ class MyGit < Formula
     # Use xdg config location
     system 'touch'    , "#{home_dir}/.config/git/config"
 
-    system 'git','config','--global','--replace-all', 'alias.lol',     '"log --graph --decorate --oneline"'
-    system 'git','config','--global','--replace-all', 'alias.lola',    '"log --graph --decorate --oneline --all"'
-    system 'git','config','--global','--replace-all', 'alias.co',      '"checkout"'
-    system 'git','config','--global','--replace-all', 'alias.ct',      '"commit"'
-    system 'git','config','--global','--replace-all', 'alias.ps',      '"push"'
-    system 'git','config','--global','--replace-all', 'alias.pl',      '"pull"'
-    system 'git','config','--global','--replace-all', 'alias.dfm',     '"diff origin/master"'
-    system 'git','config','--global','--replace-all', 'alias.st',      'status'
-    system 'git','config','--global','--replace-all', 'alias.last',    '"log -1 HEAD"'
-    system 'git','config','--global','--replace-all', 'alias.some',    '"!git fetch -a && git pull"'
-    system 'git','config','--global','--replace-all', 'alias.rb',      '"rebase -i origin/master"'
+    # Implicitly push for the current branch regardless of if that branch exists on origin
+    # http://stackoverflow.com/questions/17096311/why-do-i-need-to-explicitly-push-a-new-branch
+    system 'git', 'config', '--global', '--replace-all', 'push.default',  'current'
+
+    system 'git', 'config', '--global', '--replace-all', 'alias.lol',     '\"log --graph --decorate --oneline\"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.lola',    '"log --graph --decorate --oneline --all"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.co',      "'checkout'"
+    system 'git', 'config', '--global', '--replace-all', 'alias.ct',      'commit  a'
+    system 'git', 'config', '--global', '--replace-all', 'alias.ps',      '"push"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.pl',      '"pull"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.dfm',     '"diff origin/master"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.st',      'status'
+    system 'git', 'config', '--global', '--replace-all', 'alias.last',    '"log -1 HEAD"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.some',    '"!git fetch -a && git pull"'
+    system 'git', 'config', '--global', '--replace-all', 'alias.rb',      '"rebase -i origin/master"'
 
     # Bash completion
     system 'curl', '-fLo', "#{home_dir}/.config/bash/git-completion.bash",
